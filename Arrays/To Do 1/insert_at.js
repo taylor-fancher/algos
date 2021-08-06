c31620744e406a41417827f3ae36ccb093630c09
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 17:09:56 -0500

    finished insert_at

--- Arrays/To Do 1/insert_at.js
+++ Arrays/To Do 1/insert_at.js
@@ -1,1 +1,10 @@
 // Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+
+function insertAt(array, value, index) {
+    for (var i = array.length -1; i >= index; i --) {
+        array[i + 1] = array[i]
+    }
+    array[index] = value
+
+    console.log(array)
+}
