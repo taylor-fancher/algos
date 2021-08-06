d0c9fab9598aa67365d84741ab8e03dc39e32849
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 18:19:32 -0500

    started assignment to do 2

---
+++ Arrays/To Do 2/concat.js
@@ -0,0 +1,1 @@
+//Replicate JavaScript's concat(). Create a standalone function that accepts two arrays. Return a new array containing the first array's elements, followed by the second array's elements. Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2].
---
+++ Arrays/To Do 2/filter_range.js
@@ -0,0 +1,1 @@
+//Alan is good at breaking secret codes. One method is to eliminate values that lie outside of a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.
