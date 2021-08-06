75b529bc76533cd088571077b28bb06f215a8861
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 16:02:30 -0500

    finished push_front

--- Arrays/To Do 1/push_front.js
+++ Arrays/To Do 1/push_front.js
@@ -1,2 +1,10 @@
 // Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
 
+function pushFront(array, value) {
+    for (var i = array.length - 1; i >= 0; i --) {
+        array[i + 1] = array[i];
+    }
+    array[0] = value;
+
+    console.log(array)
+}
