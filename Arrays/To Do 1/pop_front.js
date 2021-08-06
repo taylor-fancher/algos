35a49d2a23e60cfdef1efaf64edba83dfe276ca9
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 17:05:34 -0500

    finished pop_front

--- Arrays/To Do 1/pop_front.js
+++ Arrays/To Do 1/pop_front.js
@@ -1,1 +1,11 @@
 // Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+
+function popFront(array) {
+    var removedValue = array[0];
+    for (var i = 1; i < array.length; i ++) {
+        array[i - 1] = array[i];
+    }
+    array.pop()
+
+    console.log(removedValue)
+}
