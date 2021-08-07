7fb9e53493f5ad49581f9a83f7b221a62bafc3ca
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 21:34:33 -0500

    finished filter function

--- Arrays/To Do 2/filter_range.js
+++ Arrays/To Do 2/filter_range.js
@@ -1,1 +1,13 @@
 //Alan is good at breaking secret codes. One method is to eliminate values that lie outside of a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.
+
+function filter(array, min, max) {
+    for (var i =0; i < array.length; i++) {
+        if (arr[i] < min || arr[i] > max) {
+            for (var k = i+1; k<array.length; k++) {
+                array[k-1] = array[k];
+            }
+            array.length--;
+            i--;
+        }
+    }
+}
