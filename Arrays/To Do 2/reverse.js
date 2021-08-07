081aa25f54d1406441c0a59ef5f0607f21a2c388
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 20:07:46 -0500

    finished function to reverse an array

--- Arrays/To Do 2/reverse.js
+++ Arrays/To Do 2/reverse.js
@@ -1,1 +1,11 @@
 //Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
+
+function reverse(array) {
+    for (var i = 0; i < array.length/2; i ++) {
+        var temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp
+    }
+
+    console.log(array)
+}
