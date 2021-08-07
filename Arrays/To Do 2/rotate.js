47b1cf3709babb09332d5f4dacb09809457e19ce
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 20:46:18 -0500

    working on rotate array function

--- Arrays/To Do 2/rotate.js
+++ Arrays/To Do 2/rotate.js
@@ -2,3 +2,30 @@
 //Second: allow negative shiftBy (shift L, not R).
 //Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
 //Fourth: minimize the touches of each element.
+
+
+function rotate(array, moveBy) {
+    var rotations;
+    if (moveBy > 0) {
+        rotations = moveBy % array.length;
+    } else {
+        rotations = Math.abs(moveBy) % array.length;
+    }
+    if (moveBy > 0) {
+        for (var i = 0; i < rotations; i++) {
+            var temp = array[array.length - 1];
+            for (var j = array.length - 2; j >= 0; j--) {
+                array[j + 1] = array[j];
+            }
+            array[0] = temp;
+        }
+    } else {
+        for (var i = 0; i < rotations; i++) {
+            var temp = array[0];
+            for (var j = 1; j < array.length; j++) {
+                array[j - 1] = array[j];
+            }
+            array[array.length - 1] = temp
+        }
+    }
+}
