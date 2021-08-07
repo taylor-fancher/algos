9bc88c066c766cbd517a0136be30c4cf07326597
Taylor Fancher <tfanch24@gmail.com>
2021-08-06 21:40:21 -0500

    finished concat function

--- Arrays/To Do 2/concat.js
+++ Arrays/To Do 2/concat.js
@@ -1,1 +1,15 @@
 //Replicate JavaScript's concat(). Create a standalone function that accepts two arrays. Return a new array containing the first array's elements, followed by the second array's elements. Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2].
+
+function concat(array1, array2) {
+    var newArray = [];
+    var currentIndex = 0;
+    for (var i=0; i <array1.length; i++) {
+        newArray[currentIndex] = array1[i];
+        currentIndex++;
+    }
+    for (var i = 0; i < array2.length; i++) {
+        newArray[currentIndex] = array2[i];
+        currentIndex++;
+    }
+    return newArray
+}
